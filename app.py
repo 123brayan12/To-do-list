59db71109e732be42b2a4f291b2f56217e8fe45d
123brayan12 <brayan.tamiche.g.dbb@gmail.com>
2023-06-30 17:25:10 -0400

    Listar con for

--- app.py
+++ app.py
@@ -2,8 +2,15 @@ from flask import Flask, render_template
 
 app=Flask(__name__)
 
+nombre="Brayan"
+lista_nombres=['Brayan','Jose','Mathias','Ana','Alonso']
+
 #ruta
 @app.route('/')
 #vista
-def index():
-    return render_template('base.html')
+def home():
+    return render_template('home.html',nom=nombre, lista_nombres=lista_nombres)
+
+@app.route('/about/')
+def about():
+    return render_template('about.html')
